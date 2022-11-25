c5219cfdc0bac03633fb961529879c8af2557195
Abang Laz <113807236+AbangAyoma@users.noreply.github.com>
2022-11-25 15:42:49 +0100

    Update base_converters.c

--- base_converters.c
+++ base_converters.c
@@ -5,6 +5,7 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 unsigned int convert_ubase(buffer_t *output,
 		unsigned long int num, char *base,
 		unsigned char flags, int wid, int prec);
+
 /**
  * convert_sbase - Converts a signed long to an inputted base and stores
  *                 the result to a buffer contained in a struct.
@@ -26,13 +27,16 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 
 	for (size = 0; *(base + size);)
 		size++;
+
 	if (num >= size || num <= -size)
 		ret += convert_sbase(output, num / size, base,
 				flags, wid - 1, prec - 1);
+
 	else
 	{
 		for (; prec > 1; prec--, wid--) /* Handle precision */
 			ret += _memcpy(output, &pad, 1);
+
 		if (NEG_FLAG == 0) /* Handle width */
 		{
 			pad = (ZERO_FLAG == 1) ? '0' : ' ';
@@ -40,8 +44,10 @@ unsigned int convert_sbase(buffer_t *output, long int num, char *base,
 				ret += _memcpy(output, &pad, 1);
 		}
 	}
+
 	digit = base[(num < 0 ? -1 : 1) * (num % size)];
 	_memcpy(output, &digit, 1);
+
 	return (ret);
 }
 
@@ -65,9 +71,11 @@ unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 
 	for (size = 0; *(base + size);)
 		size++;
+
 	if (num >= size)
 		ret += convert_ubase(output, num / size, base,
 				flags, wid - 1, prec - 1);
+
 	else
 	{
 		if (((flags >> 5) & 1) == 1) /* Printing a ptr address */
@@ -87,7 +95,9 @@ unsigned int convert_ubase(buffer_t *output, unsigned long int num, char *base,
 		if (((flags >> 5) & 1) == 1) /* Print 0x for ptr address */
 			ret += _memcpy(output, lead, 2);
 	}
+
 	digit = base[(num % size)];
 	_memcpy(output, &digit, 1);
+
 	return (ret);
 }
